94b2d4f150705a9f188673a0d1cd8e325acd17bf
4xeverburga <ever.burga.p@uni.pe>
2022-01-02 20:32:13 -0500

    Change1
    First Change

--- snakegame/src/pe/uni/snakegame/GamePanel.java
+++ snakegame/src/pe/uni/snakegame/GamePanel.java
@@ -1,10 +1,11 @@
 package pe.uni.snakegame;
 
 // import javax.swing.*;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.plaf.DimensionUIResource;
 // import javax.swing.text.StyleConstants.ColorConstants;
 
+import java.util.Random;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.ActionEvent;
@@ -15,42 +16,77 @@ public class GamePanel extends JPanel implements ActionListener {
 
    static final int SCREEN_WIDTH = 600;
    static final int SCREEN_HEIGHT = 600;
-   static final int UNIT_SIZE = 25; //25x25
-   static final int GAME_UNITS = (SCREEN_HEIGHT * SCREEN_HEIGHT) / UNIT_SIZE;
+   static final int UNIT_SIZE = 300; //25x25
+   static final int DELAY = 75;
+   static final int GAME_UNITS = (SCREEN_HEIGHT * SCREEN_WIDTH) / UNIT_SIZE*UNIT_SIZE;
    final int x[] = new int[GAME_UNITS];
    final int y[] = new int[GAME_UNITS];
+   int body_parts = 6;
+   int apple_eaten = 0;
+   int apple_x;
+   int apple_y;
+   boolean running = false;
 
+   //direccion
    static boolean go_left = false;
    static boolean go_right = true;
    static boolean go_up = false;
    static boolean go_down = false;
+
+   int applesEaten;
+   Timer timer;
    
    
    GamePanel(){
       
-      this.setPreferredSize(new DimensionUIResource(SCREEN_HEIGHT, SCREEN_HEIGHT));
+      this.setPreferredSize(new DimensionUIResource(SCREEN_WIDTH, SCREEN_HEIGHT));
       //122, 14, 16  
       this.setBackground(Color.black);
       this.addKeyListener(new MyKeyAdapter());
-      // draw(this);
       // startGame();
 
    }
 
+   public void move(){
 
-   public void gameComponent(Graphics graphics ){
+      for (int i = body_parts; i>0; i--) {
+         x[i] = x[i-1];
+         y[i] = y[i-1];
+      }
+
+      if (go_up) {
+         y[0] -= UNIT_SIZE;
+      } else if (go_down) {
+         y[0] += UNIT_SIZE;
+      } else if (go_left) {
+         x[0] -= UNIT_SIZE;
+      } else if (go_right) {
+         x[0] += UNIT_SIZE;
+      }
+   }
+
+   public void gameComponent(Graphics graphics){
 
    }
 
    public void startGame(){
 
+      newApple();
+      running  = true;
+      timer = new Timer(DELAY, this);
+      timer.start();
    }
 
+   // JPanel() {... paintComponent.()};
+   // Graphics g = new Graphics2d();
 
+   @Override
    public void paintComponent(Graphics graphic){
+
       super.paintComponent(graphic);
       draw(graphic);
    }
+
 
    public void draw(Graphics g){
 
@@ -58,6 +94,19 @@ public class GamePanel extends JPanel implements ActionListener {
          g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
          g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
       }
+      g.setColor(Color.red);
+      g.fillOval(apple_x, apple_y, UNIT_SIZE, UNIT_SIZE);
+      for (int i = 0; i < body_parts; i++) {
+         if (i == 0) {
+            g.setColor(Color.green);
+         }
+      }
+   }
+
+   public void newApple(){
+
+      apple_x = Random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE)) * UNIT_SIZE;
+      apple_y = Random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE)) * UNIT_SIZE;
    }
    
    @Override
@@ -66,6 +115,7 @@ public class GamePanel extends JPanel implements ActionListener {
    }
 
    public class MyKeyAdapter extends KeyAdapter{
+
       @Override
       public void keyPressed(KeyEvent event){
 
